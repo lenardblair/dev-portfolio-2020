cb7ef3b2731befc6d05174e7ebbdfc77ae76b4a6
lenardblair <49842618+lenardblair@users.noreply.github.com>
2019-10-21 13:24:54 -0500

    Update all-list.vue.js

--- components/all-list.vue.js
+++ components/all-list.vue.js
@@ -4,9 +4,9 @@ let allProjects = [
 slug:'1st Post', 
 excerpt: 'Edit and deploy new product pages for serval dozen Nissan Dealerships ', 
 content: 'I joined the Maritz-Channel Services/OppMax marketing team assigned with deploying new product pages for dozens of Nissan Dealerships during the later stages of the project. I was tasked with making  edits to HTML files arcoss various Dealer CMS platforms, each with its own in-line CSS standards. A simple, but tedious goal I found time to prototype and build variuos tools to assist with individual project goals. Most noteably, a "Code Editor"" for editing specific dealer platform pages using Vue.js. ', 
-image: 'images/nissan1.gif', 
+image: '/images/nissan1.gif', 
 link: 'https://www.rosennissanmadison.com/',
-image2: 'images/code-editor.gif', 
+image2: '/images/code-editor.gif', 
 link2: 'https://nissan'
 },
     
@@ -16,9 +16,9 @@ type:'Front-End Dev: HTML, CSS, Anime.js, Ionic',
 slug:'2nd Post', 
 excerpt: 'Design-Deploy resuable UI componets with CSS, Anime.js and Ionic. ', 
 content: ' Interactive Reading Comprehension Quiz. Bouncing Beans Version 0.5 is a free web app template for instructors to create, share and manage interactive quiz content with students. Anyone is free to use, build upon and/or re-imagine the resuable UI componets. The project was built while creating and adhering to  a strict Design System with the goal of prototyping basic UI and svg animations with Anime.js. ', 
-image: 'images/bouncingbeans1.jpg', 
+image: '/images/bouncingbeans1.jpg', 
 link: 'https://nissan',
-image2: 'images/bouncingbeans2.jpg', 
+image2: '/images/bouncingbeans2.jpg', 
 link2: 'https://nissan'
 },
     
@@ -28,9 +28,9 @@ type:'Front-End Dev: HTML, CSS, Chart.js, Bootstrap',
 slug:'3rd Post', 
 excerpt: 'Create a complete design system with UI components and prototype a full UX using InVision, followed by HTML, and Bootstrap', 
 content: 'We created a robust on-boarding portal (crisisgo.net) reducinng friction for new users and workload for HR. Admin Users/District Leaders are able to track and review the progress of invidual staff members, while users/staff members have a number of visual markers to easily keep track of their own progress, mostly made possible with Chart.js ', 
-image: 'images/crisisgo1.jpg', 
+image: '/images/crisisgo1.jpg', 
 link: 'https://crisisgo.com',
-image2: 'images/crisisgo2.jpg', 
+image2: '/images/crisisgo2.jpg', 
 link2: 'https://crisisgo.net'
 },
     
@@ -40,9 +40,9 @@ type:'Front-End Dev: HTML, CSS,  JS,  jQuery, Vue.js, Ionic',
 slug:'4th Post', 
 excerpt: 'Custom UX and UI components while adhering to site wide style guides.', 
 content: `Starting with a basic cheatsheet/repository of javascript functions for acessing the <video> element controls I created a custom media player, with a <ul> based playlist ready for a PHP loop script or Vue's "v-for" and a method/function. `, 
-image: 'images/mediaplayer1.gif', 
+image: '/images/mediaplayer1.gif', 
 link: 'https://crisisgo.com',
-image2: 'images/mediaplayer2.jpg', 
+image2: '/images/mediaplayer2.jpg', 
 link2: 'https://crisisgo.com'
 },
 
