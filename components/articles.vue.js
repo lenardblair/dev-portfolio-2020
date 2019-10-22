c9796d0d4eefd9b2be53c3f85fd07bf49c1a7f15
lenardblair <49842618+lenardblair@users.noreply.github.com>
2019-10-22 12:47:15 -0500

    Update articles.vue.js

--- components/articles.vue.js
+++ components/articles.vue.js
@@ -1,13 +1,13 @@
 let articles = [
-    { id: 1, title: 'Maritz / Nissan',
+ { id: 1, title: 'Maritz / Nissan',
  type:'Front-End Dev: HTML, Dealer CMS, Vue.js', 
 slug:'1st Post', 
 excerpt: 'Edit and deploy new product pages for serval dozen Nissan Dealerships ', 
 content: 'I joined the Maritz-Channel Services/OppMax marketing team assigned with deploying new product pages for dozens of Nissan Dealerships during the later stages of the project. I was tasked with making  edits to HTML files arcoss various Dealer CMS platforms, each with its own in-line CSS standards. A simple, but tedious goal I found time to prototype and build variuos tools to assist with individual project goals. Most noteably, a "Code Editor"" for editing specific dealer platform pages using Vue.js. ', 
 image: 'images/nissan1.gif', 
-link: 'https://nissan',
+link: 'https://www.rosennissanmadison.com/',
 image2: 'images/code-editor.gif', 
-link2: 'https://nissan'
+link2: 'https://lenardblair.github.io/editor-M-channel-services/'
 },
     
     { id: 2, 
@@ -17,9 +17,9 @@ slug:'2nd Post',
 excerpt: 'Design-Deploy resuable UI componets with CSS, Anime.js and Ionic. ', 
 content: ' Interactive Reading Comprehension Quiz. Bouncing Beans Version 0.5 is a free web app template for instructors to create, share and manage interactive quiz content with students. Anyone is free to use, build upon and/or re-imagine the resuable UI componets. The project was built while creating and adhering to  a strict Design System with the goal of prototyping basic UI and svg animations with Anime.js. ', 
 image: 'images/bouncingbeans1.jpg', 
-link: 'https://nissan',
+link: 'https://github.com/lenardblair/Bouncing_Beans_V0.5',
 image2: 'images/bouncingbeans2.jpg', 
-link2: 'https://nissan'
+link2: 'http://bit.ly/Bouncing_Beans_V1_demo'
 },
     
     { id: 3, 
@@ -41,9 +41,9 @@ slug:'4th Post',
 excerpt: 'Custom UX and UI components while adhering to site wide style guides.', 
 content: `Starting with a basic cheatsheet/repository of javascript functions for acessing the <video> element controls I created a custom media player, with a <ul> based playlist ready for a PHP loop script or Vue's "v-for" and a method/function. `, 
 image: 'images/mediaplayer1.gif', 
-link: 'https://crisisgo.com',
+link: 'https://lenardblair.github.io/vv-03/',
 image2: 'images/mediaplayer2.jpg', 
-link2: 'https://crisisgo.com'
+link2: 'https://lenardblair.github.io/vv-03/'
 },
 
     
